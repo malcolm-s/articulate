b2273895857b25dfea5765f75e2f710f82b3b90b
Malcolm Stone <malcolm.stone91@gmail.com>
2015-11-05 22:50:33 +0000

    Changing activities to reflect new intended flow.

---
+++ app/src/main/java/com/malcolmstone/articulate/GameRoundActivity.java
@@ -0,0 +1,12 @@
+package com.malcolmstone.articulate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameRoundActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_round);
+    }
+}
--- app/src/main/java/com/malcolmstone/articulate/GameStartActivity.java
+++ app/src/main/java/com/malcolmstone/articulate/GameStartActivity.java
@@ -3,13 +3,13 @@ package com.malcolmstone.articulate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
+
+import butterknife.OnClick;
 
 public class GameStartActivity extends AppCompatActivity {
     public static final String NUMBER_PLAYERS_MESSAGE = "NUMBER_PLAYERS";
@@ -21,8 +21,6 @@ public class GameStartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_start);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        bindFabClick();
     }
 
     @Override
@@ -47,35 +45,9 @@ public class GameStartActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void bindFabClick() {
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(getGoConfirmIntent());
-            }
-        });
-    }
-
-    private Intent getGoConfirmIntent() {
-        Intent intent = new Intent(this, CardsActivity.class);
-
-        //intent.putExtra(NUMBER_PLAYERS_MESSAGE, getNumberOfPlayers());
-        //intent.putExtra(NUMBER_ROUNDS_MESSAGE, getNumberOfRounds());
-
-        return intent;
-    }
-
-    private int getNumberOfPlayers() {
-        EditText txtPlayers = (EditText)findViewById(R.id.game_start_txt_players);
-
-        return Integer.parseInt(txtPlayers.getText().toString());
-    }
-
-    private int getNumberOfRounds() {
-        EditText txtRounds = (EditText)findViewById(R.id.game_start_txt_rounds);
-
-        return Integer.parseInt(txtRounds.getText().toString());
+    @OnClick(R.id.fab)
+    private void goRoundActivity(View view) {
+        Intent intent = new Intent(this, GameRoundActivity.class);
+        startActivity(intent);
     }
 }
